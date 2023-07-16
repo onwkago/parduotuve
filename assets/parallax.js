c7801107225bf7a10179d2bf8d82987a69327122
magma <dariusnavickasdn@gmail.com>
2023-07-16 13:30:36 +0300

    js

--- assets/parallax.js
+++ assets/parallax.js
@@ -1,39 +1,31 @@
+// Get all elements with class name "img-parallax"
+var imgElements = document.getElementsByClassName("img-parallax");
 
+// Iterate over each element
+Array.prototype.forEach.call(imgElements, function(img) {
+    var imgParent = img.parentElement;
 
-document.getElementByClassName("img-parallax").each(function(){
-    let img = this;
-    let imgParent = this.parent();
-    function parallaxImg () {
-        console.info('bybys kiausai');
-        let speed = img.data('speed');
-        let imgY = imgParent.offset().top;
-        let winY = this.scrollTop();
-        let winH = this.height();
-        let parentH = imgParent.innerHeight();
-
-
-
-        let winBottom = winY + winH;
-
+    // Define the parallaxImg function
+    function parallaxImg() {
+        var speed = img.getAttribute('data-speed');
+        var imgY = imgParent.offsetTop;
+        var winY = window.scrollY || window.pageYOffset;
+        var winH = window.innerHeight;
+        var parentH = imgParent.clientHeight;
+        var winBottom = winY + winH;
+        var imgPercent;
 
         if (winBottom > imgY && winY < imgY + parentH) {
-
-            let imgBottom = ((winBottom - imgY) * speed);
-
-            let imgTop = winH + parentH;
-
-            let imgPercent = ((imgBottom / imgTop) * 100) + (50 - (speed * 50));
+            var imgBottom = (winBottom - imgY) * speed;
+            var imgTop = winH + parentH;
+            imgPercent = (imgBottom / imgTop) * 100 + (50 - speed * 50);
         }
-        img.css({
-            top: imgPercent + '%',
-            transform: 'translate(-50%, -' + imgPercent + '%)'
-        });
+
+        img.style.top = imgPercent + '%';
+        img.style.transform = 'translate(-50%, -' + imgPercent + '%)';
     }
-    document.on({
-        scroll: function () {
-            parallaxImg();
-        }, ready: function () {
-            parallaxImg();
-        }
-    });
+
+    // Attach event listeners to scroll and DOM ready events
+    document.addEventListener('scroll', parallaxImg);
+    window.addEventListener('DOMContentLoaded', parallaxImg);
 });
